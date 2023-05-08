adffb8019b25fa39da253b824e7e0b0fce1dff88
Temm <samumert@gmail.com>
2023-05-08 03:25:34 +0200

    Add build system
    rebase: ' over "

---
+++ src/scripts/hello.ts
@@ -0,0 +1,3 @@
+import fs from "fs"
+
+Chat.log(1);
---
+++ tsup.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'tsup'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const entryFiles: string[] = [];
+
+function isTsOrJsFile(dirent: fs.Dirent) {
+  if (!dirent.isFile()) return false;
+  const ext = path.extname(dirent.name);
+  return ext === '.ts' || ext === '.js';
+}
+
+function processFolder(folderPath: string) {
+  const folderContents = fs.readdirSync(folderPath, { withFileTypes: true });
+  const folderName = path.basename(folderPath);
+
+  for (const dirent of folderContents) {
+    if (isTsOrJsFile(dirent)) {
+      const ext = path.extname(dirent.name);
+      const baseName = path.basename(dirent.name, ext);
+
+      if (baseName === folderName) {
+        entryFiles.push(path.join(folderPath, dirent.name));
+        return;
+      }
+    }
+  }
+
+  for (const dirent of folderContents) {
+    const fullPath = path.join(folderPath, dirent.name);
+    if (isTsOrJsFile(dirent)) {
+      entryFiles.push(fullPath);
+    } else if (dirent.isDirectory()) {
+      processFolder(fullPath);
+    }
+  }
+}
+
+processFolder('./src/scripts/');
+
+console.log('Using entry files: ' + entryFiles.join(', '));
+
+export default defineConfig({
+    entry: entryFiles.map(e=>`./${e.replace(/\\/g, '/')}`),
+    'tsconfig': './src/tsconfig.json',
+    dts: true,
+})
