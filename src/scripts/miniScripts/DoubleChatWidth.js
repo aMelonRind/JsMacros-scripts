fd93efa1c2a5bf8fa8d497253532369b214761c5
aMelonRind <kong49123@gmail.com>
2023-08-24 00:14:16 +0800

    refresh chat after setWidth

--- src/scripts/miniScripts/DoubleChatWidth.js
+++ src/scripts/miniScripts/DoubleChatWidth.js
@@ -18,17 +18,25 @@ let was = 1.0
 
 JsMacros.on('OpenScreen', JavaWrapper.methodToJava(e => {
   if (e.screenName === 'Chat') {
-    if (!isOpen) was = optionValue.get(widthOption)
-    optionValue.set(widthOption, new Double(was * multiplier))
+    if (!isOpen) was = Math.min(1, optionValue.get(widthOption))
     isOpen = true
+    setWidth(was * multiplier)
   } else if (isOpen) {
-    optionValue.set(widthOption, new Double(was))
+    setWidth(was)
     isOpen = false
   }
 }))
 
 event.stopListener = JavaWrapper.methodToJava(() => {
-  if (isOpen) optionValue.set(widthOption, new Double(was))
+  if (isOpen) setWidth(was)
 })
+
+/**
+ * @param {number} value 
+ */
+function setWidth(value) {
+  optionValue.set(widthOption, new Double(value))
+  Chat.getHistory().refreshVisible()
+}
 
 module.exports = {}
