ef561f013dfc93f56ea763f0bb5bf70e30e1afa9
aMelonRind <kong49123@gmail.com>
2023-09-03 12:28:12 +0800

    add converter for zhuyin(english) to chinese

--- src/scripts/miniScripts/PosFiller.js
+++ src/scripts/miniScripts/PosFiller.js
@@ -11,21 +11,24 @@
 
 if (!World.isWorldLoaded()) JsMacros.waitForEvent('ChunkLoad')
 
-let block
-/** @type {SuggestingListener[]} */
 const listeners = [
   { // bpos: block pos
+    /** @type {BlockDataHelper?} */
+    block: null,
+    /** @type {OpenChatScreenListener} */
     onOpenChatScreen() {
-      block = Player.rayTraceBlock(8, false)
+      this.block = Player.rayTraceBlock(8, false)
     },
+    /** @type {KeywordListener} */
     onKeyword(keyword, sym) {
       if (keyword !== 'bpos') return
-      if (!(block ||= Player.rayTraceBlock(8, false))) return
-      if (sym === ',') return `${block.getX()}, ${block.getY()}, ${block.getZ()}`
-      if (sym === '')  return `${block.getX()} ${block.getY()} ${block.getZ()} `
+      if (!(this.block ??= Player.rayTraceBlock(8, false))) return
+      if (sym === ',') return `${this.block.getX()}, ${this.block.getY()}, ${this.block.getZ()}`
+      if (sym === '')  return `${this.block.getX()} ${this.block.getY()} ${this.block.getZ()} `
     }
   },
   { // pos: player pos
+    /** @type {KeywordListener} */
     onKeyword(keyword, sym) {
       if (keyword !== 'pos') return
       const pos = Player.getPlayer().getPos()
@@ -37,6 +40,7 @@ const listeners = [
     }
   },
   { // cpos: camera pos
+    /** @type {KeywordListener} */
     onKeyword(keyword, sym) {
       if (keyword !== 'cpos') return
       const cam = Client.getMinecraft().field_1773.method_19418().method_19326() // .gameRenderer.getCamera().getPos()
@@ -48,13 +52,109 @@ const listeners = [
     }
   },
   { // caps convert to lower case
+    /** @type {ChangeListener} */
     onChange(text, cursor) {
       text = text.slice(0, cursor)
-      if (/[A-Z]/.test(text) && /^[A-Z ,.!?]+$/.test(text))
+      if (text.length > 3 && /[A-Z]/.test(text) && /^[A-Z ,.!?]+$/.test(text))
         return new Suggestion(0, cursor, text.toLowerCase())
     }
   },
+  { // can convert zhuyin(english) to chinese
+    // /** @type {ChangeListener} */
+    // onChange(text, cursor) {
+    //   const final = []
+    //   if (cursor < text.length && this.regex.test(text.charAt(cursor))) {
+    //     const res = this.fromZhuyin(text.slice(cursor).match(this.regex)[0])
+    //     if (res) final.push(...res[0].map((text, i) => new Suggestion(cursor, cursor + res[1][i], text)))
+    //   }
+    //   const match = text.match(this.regex)
+    //   if (match && match.index !== cursor) {
+    //     const res = this.fromZhuyin(match[0])
+    //     if (res) final.push(...res[0].map((text, i) => new Suggestion(match.index, match.index + res[1][i], text)))
+    //   }
+    //   return final
+    // },
+
+    // regex: /[ A-Za-z0-9,./;-]+/,
+
+    // /** @type {string[]} */
+    // cacheIndex: [],
+    // /** @type {Record<string, string[] | null>} */
+    // cache: {},
+
+    // /**
+    //  * @param {string} key 
+    //  * @param {string[] | null} value 
+    //  * @returns 
+    //  */
+    // storeCache(key, value) {
+    //   this.cacheIndex.push(key)
+    //   this.cache[key] = this.clone(value)
+    //   if (this.cacheIndex.length > 256) this.cacheIndex.splice(0, 50).forEach(k => delete this.cache[key])
+    //   return value
+    // },
+
+    // /**
+    //  * @param {string[] | null} value 
+    //  * @returns {string[] | null}
+    //  */
+    // clone(value) {
+    //   return Array.isArray(value) ? value.slice() : value
+    // },
+
+    // /**
+    //  * converts zhuyin(english) to chinese characters using google input tools
+    //  * @param {string} zhuyin input zhuyin that accidently typed in english
+    //  * @param {number} count max guesses
+    //  * @returns {[string[], number[]]}
+    //  */
+    // fromZhuyin(zhuyin, count = 3) {
+    //   if (!zhuyin) return null
+    //   if (typeof count !== 'number' || count < 1 || isNaN(count)) count = 1
+    //   if (count > 16) count = 16
+    //   const orderres = this.rearrangeZhuyin(zhuyin)
+    //   if (!orderres.res) return null
+    //   if (this.cacheIndex.includes(orderres.res)) {
+    //     if (!this.cache[orderres.res]) return null
+    //     return [this.clone(this.cache[orderres.res]), this.cache[orderres.res].map(s => orderres.indexes[s.length - 1])]
+    //   }
+    //   zhuyin = orderres.res.replaceAll(' ', '=')
+    //   const res = Request.get(`https://inputtools.google.com/request?text=${encodeURIComponent(zhuyin.replaceAll(',', '%2C'))}&itc=zh-hant-t-i0-und&num=${count}&ie=utf-8&oe=utf-8&app=minecraft-chat`)
+    //   if (res.responseCode !== 200) return null
+    //   try {
+    //     const json = JSON.parse(res.text())
+    //     if (json[0] !== 'SUCCESS') return null
+    //     if (json[1][0][1].length === 0) return this.storeCache(orderres.res, null)
+    //     return [this.storeCache(orderres.res, json[1][0][1]), json[1][0][1].map(s => orderres.indexes[s.length - 1])]
+    //   } catch (e) {}
+    //   return null
+    // },
+
+    // /**
+    //  * rearranges the zhuyin(english) to correct order
+    //  * @param {string} zhuyin 
+    //  * @returns 
+    //  */
+    // rearrangeZhuyin(zhuyin) {
+    //   let res = ''
+    //   const indexes = []
+    //   const inp = new Array(3).fill(null)
+    //   zhuyin = zhuyin.toLowerCase()
+    //   for (let index = 0, char; index < zhuyin.length; index++) {
+    //     char = zhuyin[index]
+    //     if (' 6347'.includes(char)) {
+    //       res += inp.join('') + char
+    //       indexes.push(index + 1)
+    //       inp.fill(null)
+    //     } else if ('1qaz2wsxedcrfv5tgbyhn'.includes(char)) inp[0] = char
+    //     else if ('ujm'.includes(char)) inp[1] = char
+    //     else if ('8ik,9ol.0p;/-'.includes(char)) inp[2] = char
+    //   }
+    //   return { res, indexes }
+    // }
+  },
   // { // test: multiple string range is supported
+  //   /** @type {ChangeListener} */
   //   onChange(s, c) {
   //     s = s.slice(0, c)
   //     const res = []
@@ -66,6 +166,7 @@ const listeners = [
   //   }
   // },
   // { // test: will suggest after picked a suggest
+  //   /** @type {KeywordListener} */
   //   onKeyword(k) {
   //     if (k === 'pos') return 'bpos'
   //   }
@@ -79,9 +180,9 @@ const onChanges = []
 /** @type {KeywordListener[]} */
 const onKeywords = []
 listeners.forEach(l => {
-  if (l.onOpenChatScreen) onOpenChatScreens.push(l.onOpenChatScreen)
-  if (l.onChange) onChanges.push(l.onChange)
-  if (l.onKeyword) onKeywords.push(l.onKeyword)
+  if (l.onOpenChatScreen) onOpenChatScreens.push(l.onOpenChatScreen.bind(l))
+  if (l.onChange) onChanges.push(l.onChange.bind(l))
+  if (l.onKeyword) onKeywords.push(l.onKeyword.bind(l))
 })
 
 const StringRange = Java.type('com.mojang.brigadier.context.StringRange')
@@ -99,7 +200,11 @@ let currentText = ''
 
 JsMacros.on('OpenScreen', JavaWrapper.methodToJava(e => {
   if (e.screenName !== 'Chat') return
-  onOpenChatScreens.forEach(cb => cb(e.screen))
+  onOpenChatScreens.forEach(cb => {
+    try {
+      cb(e.screen)
+    } catch (e) { logerr(e) }
+  })
   new Promise((res, rej) => {
     e.screen.setOnKeyPressed(JavaWrapper.methodToJava(res))
     e.screen.setOnClose(JavaWrapper.methodToJava(rej))
@@ -130,13 +235,19 @@ function triggerSuggest(screen, input, text) {
   const keywordMatch = text.slice(0, cursor).match(/\$?\b(\w+)(\W*)$/)
 
   /** @type {Suggestion[]} */
-  const res = onChanges.flatMap(cb => cb(text, cursor))
-    .filter(/** @type {Filter<Suggestion>} */ (s => s instanceof Suggestion))
+  const res = onChanges.flatMap(cb => {
+    try {
+      return cb(text, cursor)
+    } catch (e) { logerr(e) }
+  }).filter(/** @type {Filter<Suggestion>} */ (s => s instanceof Suggestion))
     .filter(s => !s.discard)
   if (keywordMatch) {
     const [, keyword, sym] = keywordMatch
-    const kres = onKeywords.flatMap(cb => cb(keyword, sym))
-      .filter(/** @type {Filter<string>} */ (v => typeof v === 'string'))
+    const kres = onKeywords.flatMap(cb => {
+      try {
+        return cb(keyword, sym)
+      } catch (e) { logerr(e) }
+    }).filter(/** @type {Filter<string>} */ (v => typeof v === 'string'))
     if (kres.length) {
       const start = keywordMatch.index ?? 0
       const end = start + keywordMatch[0].length
@@ -170,6 +281,10 @@ function triggerSuggest(screen, input, text) {
     false // narrateFirstSuggestion
   ))
   InputSuggestor.method_23933(true) // .setWindowActive()
+}
+
+function logerr(e) {
+  Chat.log(e.toString())
 }
 
 /**
