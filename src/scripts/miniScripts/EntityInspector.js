3d4deb498fa1ad6310c01109456c249b7e03ed41
aMelonRind <kong49123@gmail.com>
2024-08-28 02:08:38 +0800

    update EntityInspector

--- src/scripts/miniScripts/EntityInspector.js
+++ src/scripts/miniScripts/EntityInspector.js
@@ -1,21 +1,14 @@
-
+//@ts-check
 // able to get entity info/item
 // look at entity, use alt + middle mouse to trigger
 JsMacros.assertEvent(event, 'Service')
 module.exports = 0
 
-if (!World.isWorldLoaded()) JsMacros.waitForEvent('ChunkLoad')
+if (!World.isWorldLoaded())
+  JsMacros.waitForEvent('ChunkLoad')
 
-const RegistryHelper     = Java.type('xyz.wagyourtail.jsmacros.client.api.classes.RegistryHelper')
 const LivingEntityHelper = Java.type('xyz.wagyourtail.jsmacros.client.api.helpers.world.entity.LivingEntityHelper')
-const NBTElementHelper   = Java.type('xyz.wagyourtail.jsmacros.client.api.helpers.NBTElementHelper')
-const ItemStackHelper    = Java.type('xyz.wagyourtail.jsmacros.client.api.helpers.inventory.ItemStackHelper')
-const ItemStack          = Java.type('net.minecraft.class_1799')
 
-/** @type {(text: string) => number} mc.textRenderer.getWidth */
-const getTextWidth = Client.getMinecraft().field_1772.method_1727
-/** @type {ScriptScreen} */
-let sc
 let lastNbtPath = ''
 let inspectSelf = false
 
@@ -46,125 +39,109 @@ JsMacros.on('Key', JavaWrapper.methodToJava(e => {
  */
 function openEntityInspectScreen(entity) {
   if (!entity) return
-  sc = Hud.createScreen('Entity Inspector', false)
+  const sc = Hud.createScreen('Entity Inspector', false)
   sc.setOnInit(JavaWrapper.methodToJava(() => {
-    const cx = Math.floor(sc.getWidth()  / 2)
+    const width = sc.getWidth()
+    const cx = Math.floor(width / 2)
     const cy = Math.floor(sc.getHeight() / 2)
     // add title
-    const title = Chat.createTextHelperFromJSON(`{
-      "text": "Inspecting ",
-      "extra": [${entity.getName().getJson()}]
-    }`)
-    sc.addText(
-      title,
-      cx - Math.floor(getTextWidth(title.getStringStripFormatting()) / 2),
-      cy - 50,
-      0xFFFFFF,
-      true
-    )
+    const title = Chat.createTextBuilder().append('Inspecting ').append(entity.getName()).build()
+    sc.addText(title, cx - Math.floor(title.getWidth() / 2), cy - 50, 0xFFFFFF, true)
     // add entity type
-    sc.addText(
-      entity.getType(),
-      cx - Math.floor(getTextWidth(entity.getType()) / 2),
-      cy - 40,
-      0xAFAFAF,
-      true
-    )
-    if (isLiving(entity)) {
+    const type = entity.getType()
+    sc.addText(type, cx - Math.floor(Chat.getTextWidth(type) / 2), cy - 40, 0xAFAFAF, true)
+    if (entity instanceof LivingEntityHelper) {
       // add armoritems
-      addItem(cx - 35, cy - 28, entity.getHeadArmor(),  'No Helmet!')
-      addItem(cx - 17, cy - 28, entity.getChestArmor(), 'No Chestplate!')
-      addItem(cx +  1, cy - 28, entity.getLegArmor(),   'No Leggings!')
-      addItem(cx + 19, cy - 28, entity.getFootArmor(),  'No Boots!')
+      addItem(sc, cx - 35, cy - 28, entity.getHeadArmor(),  'No Helmet!')
+      addItem(sc, cx - 17, cy - 28, entity.getChestArmor(), 'No Chestplate!')
+      addItem(sc, cx +  1, cy - 28, entity.getLegArmor(),   'No Leggings!')
+      addItem(sc, cx + 19, cy - 28, entity.getFootArmor(),  'No Boots!')
       // add handitems
-      const handsX = [cx - 35, cx + 19]
-      if (Client.getGameOptions().isRightHanded()) handsX.reverse()
-      addItem(handsX[0], cy - 8, entity.getMainHand(), 'No MainHand Item!')
-      addItem(handsX[1], cy - 8, entity.getOffHand(),  'No OffHand Item!')
+      const handsX = [cx + 19, cx - 35]
+      if (!Client.getGameOptions().getSkinOptions().isRightHanded()) handsX.reverse()
+      addItem(sc, handsX[0], cy - 8, entity.getMainHand(), 'No MainHand Item!')
+      addItem(sc, handsX[1], cy - 8, entity.getOffHand(),  'No OffHand Item!')
+    }
+    // item frame
+    if (entity.is('item_frame', 'glow_item_frame')) {
+      addItem(sc, cx - 7, cy - 28, entity.getItem(), 'No Framed Item!')
     }
     // add nbt inspector
+    // might do syntax highlighting and multi line in the future..
+    // doubt that it's needed tho.
     const nbtOutput = sc.addText('NBT Inspector', cx - 78, cy + 36, 0xFFFFFF, true)
     const nbtInput = sc.addTextInput(cx - 80, cy + 16, 160, 16, '',
       JavaWrapper.methodToJava(txt => {
         lastNbtPath = txt
-        if (txt === '') return nbtOutput.setText('NBT Inspector')
+        if (txt === '') return putNbtOutput(nbtOutput, width, 'NBT Inspector')
         /** @type {NBTElementHelper} */
         let nbt = entity.getNBT()
         const path = txt.split(/[\.\[\]\s]/).filter(v => v)
-        try {
-          while (nbt && (path[1] || /^\d+$/.test(path[0]))) {
-            // @ts-ignore
-            if (/^\d+$/.test(path[0])) nbt = nbt.get(parseInt(path.shift()))
-            // @ts-ignore
-            else nbt = nbt.get(path.shift())
-          }
-        } catch (/** @type {*} */ e) {
-          return nbtOutput.setText("can't parse output: " + e.message)
+        let pop = path.pop()
+        if (!pop) {
+          return putNbtOutput(nbtOutput, width, 'NBT Inspector')
+        } else if (/^\d+$/.test(pop)) {
+          path.push(pop)
+          pop = undefined
         }
-        /** @type {NBTElementHelper | string} */
+        try {
+          for (const key of path.map(v => /^\d+$/.test(v) ? parseInt(v) : v)) {
+            //@ts-ignore
+            nbt = nbt.get(key)
+          }
+        } catch (e) {
+          return putNbtOutput(nbtOutput, width, "can't parse output: " + e.message)
+        }
+        /** @type {NBTElementHelper | string | null} */
         let snbt = nbt
-        if (snbt?.isCompound() && path[0]) {
+        if (snbt?.isCompound() && pop) {
           const keys = snbt.getKeys()
-          if (keys.contains(path[0])) snbt = snbt.get(path[0])
-          else {
-            const filtered = keys.toArray().filter(k => k.toLowerCase().startsWith(path[0].toLowerCase()))
+          if (keys.contains(pop)) {
+            snbt = snbt.get(pop)
+          } else {
+            const lcpop = pop.toLowerCase()
+            const filtered = keys.toArray().filter(k => k.toLowerCase().startsWith(lcpop))
             if (filtered.length === 1) {
               snbt = snbt.get(filtered[0])
               if (lastNbtPath.endsWith(' ')) // press space to autocomplete
-              nbtInput.setText(lastNbtPath.trimEnd() + filtered[0].slice(path[0].length) + '.')
+              nbtInput.setText(lastNbtPath.trimEnd().slice(0, -pop.length) + filtered[0] + '.')
             }
             else snbt = filtered.join(', ')
           }
         }
         if (typeof snbt === 'string') {}
-        else if (!snbt)                 snbt = 'null'
-        else if (snbt.isCompound())     snbt = snbt.toString()
-        // else if (snbt.isPossiblyUUID()) snbt = snbt.asUUID().toString()
-        else if (snbt.isList())         snbt = snbt.toString()
-        else if (snbt.isNull())         snbt = 'null'
-        else if (snbt.isNumber())       snbt = snbt.asNumber().toString()
-        else if (snbt.isString())       snbt = snbt.asString()
+        else if (!snbt)             snbt = 'undefined'
+        else if (snbt.isCompound()) snbt = snbt.asString()
+        else if (snbt.isString())   snbt = snbt.asString()
+        else if (snbt.isNumber())   snbt = snbt.asNumber().toString()
+        else if (snbt.isList())     snbt = snbt.asListHelper().asUUID()?.toString() ?? snbt.asString()
+        else if (snbt.isNull())     snbt = 'null'
         else snbt = snbt.toString()
-        if (snbt?.startsWith?.('NBT')) snbt = toSnbt(snbt)
-        nbtOutput.setText(`${snbt}`)
+        putNbtOutput(nbtOutput, width, `${snbt}`)
       })
     ).setText(lastNbtPath)
-    // extra: item frame
-    if (entity.getType().endsWith('item_frame'))
-      // @ts-ignore
-      addItem(cx - 7, cy - 28, entity.getNBT().get('Item'), 'No Framed Item!')
   }))
   Hud.openScreen(sc)
 }
 
 /**
  * add an item with hover event and click event
+ * @param {ScriptScreen} sc 
  * @param {number} x 
  * @param {number} y 
  * @param {ItemStackHelper} item 
  * @param {string} textIfEmpty 
  */
-function addItem(x, y, item, textIfEmpty = 'No Item!') {
-  if (item instanceof NBTElementHelper) item = ItemStack.method_57359(RegistryHelper.NBT_OPS_UNLIMITED, item.getRaw()) // .fromNbtOrEmpty()
-  if (item && !(item instanceof ItemStackHelper)) item = new ItemStackHelper(item)
+function addItem(sc, x, y, item, textIfEmpty = 'No Item!') {
+  const builder = Chat.createTextBuilder().append('空空')
   if (!item || item.isEmpty()) {
     sc.addItem(x, y, 'minecraft:barrier')
-    const txt = Chat.createTextBuilder()
-      .append('空空')
-      .withShowTextHover(typeof textIfEmpty === 'string' ?
-        Chat.createTextHelperFromString(textIfEmpty) : textIfEmpty
-      )
-      .build()
-    sc.addText(txt, x - 2, y - 2, 0x05000000, false)
-    sc.addText(txt, x - 2, y + 6, 0x05000000, false)
-    return
+    builder.withShowTextHover(Chat.createTextHelperFromString(textIfEmpty))
+  } else {
+    sc.addItem(x, y, item)
+    builder.withShowItemHover(item).withCustomClickEvent(JavaWrapper.methodToJava(getItem(item)))
   }
-  sc.addItem(x, y, item)
-  const txt = Chat.createTextBuilder()
-    .append('空空')
-    .withShowItemHover(item)
-    .withCustomClickEvent(JavaWrapper.methodToJava(getItem(item)))
-    .build()
+  const txt = builder.build()
   sc.addText(txt, x - 2, y - 2, 0x05000000, false)
   sc.addText(txt, x - 2, y + 6, 0x05000000, false)
 }
@@ -176,47 +153,46 @@ function getItem(item) {
   return () => {
     if (Player.getGameMode() === 'creative') {
       const InvScreen = Java.type('net.minecraft.class_490')
-      const Inventory = Java.type('xyz.wagyourtail.jsmacros.client.api.classes.Inventory')
-      const inv = Inventory.create(new InvScreen(Player.getPlayer().getRaw()))
-      const slot = Java.from(inv.getMap().hotbar).concat(inv.getMap().main).find(slot => {
+      const Inventory = Java.type('xyz.wagyourtail.jsmacros.client.api.classes.inventory.Inventory')
+      const inv = Inventory.create(new InvScreen(Player.getPlayer()?.getRaw()))
+      const slot = inv?.getSlots('hotbar', 'main').find(slot => {
         return inv.getSlot(slot).isEmpty()
       })
       if (slot !== undefined) {
         const CreativeInventoryActionC2SPacket = Java.type("net.minecraft.class_2873")
-        const ClientPlayNetworkHandler = Client.getMinecraft().method_1562()
-        ClientPlayNetworkHandler.method_2883(new CreativeInventoryActionC2SPacket(slot, item.getRaw()))
+        Client.sendPacket(new CreativeInventoryActionC2SPacket(slot, item.getRaw()))
         return
       }
-    } else if (Player.getPlayer().getRaw().method_5687(2)) // .hasPermissionLevel
-      return Chat.say(`/give @s ${item.getItemId()}${toSnbt(item.getNBT())}`)
+    } else if (Player.getPlayer()?.getRaw().method_5687(2)) { // .hasPermissionLevel
+      return Chat.say(`/give @s ${item.getItemId()}${item.getNBT().asString()}`)
+    }
 
-    Chat.log(Chat.createTextHelperFromJSON(`{
-      "text": "Don't have /give permission to get ",
-      "extra": [${item.getName().getJson()}, {
-        "text": "\nClick here to copy tags to clipboard.",
-        "clickEvent": {
-          "action": "copy_to_clipboard",
-          "value": "${item.getItemId()}${toSnbt(item.getNBT()).replace(/"/g, '\\"')}"
-        }
-      }]
-    }`))
+    Chat.log(
+      Chat.createTextBuilder()
+        .append("Don't have /give permission to get ").append(item.getName()).append('\n')
+        .append('Click here to copy tags to clipboard.')
+        .withClickEvent('COPY_TO_CLIPBOARD', `${item.getItemId()}${item.getNBT().asString().replace(/"/g, '\\"')}`)
+    )
   }
 }
 
 /**
- * @param {EntityHelper} entity 
- * @returns {entity is LivingEntityHelper}
+ * @param {Text} out 
+ * @param {number} maxwidth 
+ * @param {string} str 
  */
-function isLiving(entity) {
-  return entity instanceof LivingEntityHelper
-}
-
-/**
- * @param {NBTElementHelper | string} nbt 
- */
-function toSnbt(nbt) {
-  if (nbt instanceof NBTElementHelper) nbt = nbt.toString()
-  if (typeof nbt !== 'string') return ''
-  nbt = nbt.match(/^[^\{]*(\{.*\})$/)?.[1] ?? ''
-  return nbt.startsWith('{{') ? nbt.slice(1, -1) : nbt
+function putNbtOutput(out, maxwidth, str) {
+  maxwidth -= 20
+  if (Chat.getTextWidth(str) > maxwidth) {
+    let err = Math.floor(str.length / 2)
+    let index = str.length
+    while (err > 1) {
+      if (Chat.getTextWidth(str.slice(0, index - err)) > maxwidth) {
+        index -= err
+      }
+      err = Math.floor(err / 2)
+    }
+    str = str.slice(0, index)
+  }
+  out.setText(str).alignHorizontally('center')
 }
