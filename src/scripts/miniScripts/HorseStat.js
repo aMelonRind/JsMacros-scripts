ed5f21aa6dddb0dc49dc4d0696236a601f0e078c
aMelonRind <kong49123@gmail.com>
2024-08-29 10:49:23 +0800

    rewrite HorseStat

--- src/scripts/miniScripts/HorseStat.js
+++ src/scripts/miniScripts/HorseStat.js
@@ -1,127 +1,83 @@
-
+//@ts-check
 // show horse stat when you look at it
 JsMacros.assertEvent(event, 'Service')
 module.exports = 0
 
-if (!World.isWorldLoaded()) JsMacros.waitForEvent('ChunkLoad')
+if (!World.isWorldLoaded())
+  JsMacros.waitForEvent('ChunkLoad')
+
 const d2d = Hud.createDraw2D()
-const orig = {
-  x: 0,
-  y: 0,
-  d2dsize: {
-    w: 0,
-    h: 0
-  },
-  bar: {
-    orig: 0,
-    sizeMultiply: 1.32
-  }
+const graphic = Hud.createDraw2D()
+
+const elem = {
+  background: graphic.addRect(0, 0, 160, 76, 0x333333),
+  title:      graphic.addText('Name',  5, 5, 0xFFFFFF, false),
+  speedIcon:  graphic.addImage(4, 16, 16, 16, 'textures/mob_effect/speed.png',        0, 0, 16, 16, 16, 16),
+  jumpIcon:   graphic.addImage(4, 36, 16, 16, 'textures/mob_effect/jump_boost.png',   0, 0, 16, 16, 16, 16),
+  healthIcon: graphic.addImage(4, 56, 16, 16, 'textures/mob_effect/health_boost.png', 0, 0, 16, 16, 16, 16),
+  speedText:  graphic.addText('Speed: ',  24, 19, 0xFFFFFF, false),
+  jumpText:   graphic.addText('Jump: ',   24, 39, 0xFFFFFF, false),
+  healthText: graphic.addText('Health: ', 24, 59, 0xFFFFFF, false),
+  speedBar:   graphic.addRect(24, 28, 156, 29, 0xFFFFFF),
+  jumpBar:    graphic.addRect(24, 48, 156, 49, 0xFFFFFF),
+  healthBar:  graphic.addRect(24, 68, 156, 69, 0xFFFFFF)
 }
-const Elem = {
-  background: d2d.addRect(0, 0, 160, 76, 0x333333),
-  title:      d2d.addText('Name',  5, 5, 0xFFFFFF, false),
-  speedIcon:  d2d.addImage(4, 16, 16, 16, 'textures/mob_effect/speed.png',        0, 0, 16, 16, 16, 16),
-  jumpIcon:   d2d.addImage(4, 36, 16, 16, 'textures/mob_effect/jump_boost.png',   0, 0, 16, 16, 16, 16),
-  healthIcon: d2d.addImage(4, 56, 16, 16, 'textures/mob_effect/health_boost.png', 0, 0, 16, 16, 16, 16),
-  speedText:  d2d.addText('Speed: ',  24, 19, 0xFFFFFF, false),
-  jumpText:   d2d.addText('Jump: ',   24, 39, 0xFFFFFF, false),
-  healthText: d2d.addText('Health: ', 24, 59, 0xFFFFFF, false),
-  speedBar:   d2d.addRect(24, 28, 156, 29, 0xFFFFFF),
-  jumpBar:    d2d.addRect(24, 48, 156, 49, 0xFFFFFF),
-  healthBar:  d2d.addRect(24, 68, 156, 69, 0xFFFFFF)
-}
-/** @type {['speed', 'jump', 'health']} */
-const sjh = ['speed', 'jump', 'health']
-/** @type {[number, number, number, number, number, number]} */// @ts-ignore
-const rankGrading = [0,        40,       70,       80,       90,       95].reverse()
-/** @type {[number, number, number, number, number, number]} */
+
+const rankGrading = [0,        40,       70,       80,       90,       95,      Infinity]
 const rankColor   = [0x808080, 0xFFFFFF, 0x00FF00, 0xFF8000, 0xFF00FF, 0xE80000]
 //                   gray      white     green     gold      purple    red
-/** @type {string?} */
-let horseid = null
+/** @type {EntityHelper?} */
+let lastHorse = null
 let keep = 0
 
-d2d.setOnInit(JavaWrapper.methodToJava(() => {
-  if (orig.d2dsize.w !== d2d.getWidth() || orig.d2dsize.h !== d2d.getHeight()) { // size is 160x76
-    orig.d2dsize.w = d2d.getWidth()
-    orig.d2dsize.h = d2d.getHeight()
-    orig.x = Elem.background.x1 = Math.floor(orig.d2dsize.w / 2) - 79
-    orig.y = Elem.background.y1 = Math.floor(orig.d2dsize.h / 2) + 20
-    orig.bar.orig = orig.x + 24
-    Elem.background.x2 = orig.x + 160
-    Elem.background.y2 = orig.y + 76
-    Elem.title.x       = orig.x + 5
-    Elem.title.y       = orig.y + 5
-    Elem.speedIcon.x =  Elem.jumpIcon.x =  Elem.healthIcon.x = orig.x + 4
-    Elem.speedText.x =  Elem.jumpText.x =  Elem.healthText.x =
-    Elem.speedBar.x1 =  Elem.jumpBar.x1 =  Elem.healthBar.x1 = orig.x + 24
-    Elem.speedBar.x2 =  Elem.jumpBar.x2 =  Elem.healthBar.x2 = orig.x + 155
-    Elem.speedIcon.y = (Elem.jumpIcon.y = (Elem.healthIcon.y = orig.y + 56) - 20) - 20
-    Elem.speedText.y = (Elem.jumpText.y = (Elem.healthText.y = orig.y + 59) - 20) - 20
-    Elem.speedBar.y1 = (Elem.jumpBar.y1 = (Elem.healthBar.y1 = orig.y + 68) - 20) - 20
-    Elem.speedBar.y2 = (Elem.jumpBar.y2 = (Elem.healthBar.y2 = orig.y + 69) - 20) - 20
+d2d.setOnInit(JavaWrapper.methodToJavaAsync(2, () => {
+  d2d.addDraw2D(graphic, Math.floor(d2d.getWidth() / 2) - 79, Math.floor(d2d.getHeight() / 2) + 20, 160, 76)
+  if (graphic.getElements().isEmpty()) { // in case the element got cleared
+    for (const e of Object.values(elem)) graphic.reAddElement(e)
   }
-  Object.values(Elem).forEach(e => {d2d.reAddElement(e)})
 }))
 
 JsMacros.on('Tick', JavaWrapper.methodToJava(() => {
-  const horse = Player.getInteractionManager().getTargetedEntity()
-  if (horseid && (horse?.getUUID() === horseid)) return keep = 8
-  if (horse?.getType() !== 'minecraft:horse')
-    if (!horseid || keep-- > 0) return
-    else {
+  const horse = Player.getInteractionManager()?.getTargetedEntity()
+  if (lastHorse?.equals(horse)) return keep = 8
+  if (!horse?.is('horse')) {
+    if (lastHorse && keep-- <= 0) {
       d2d.unregister()
-      horseid = null
-      return
+      lastHorse = null
     }
-  /** @type {Record<string, number>} */
-  const rawAttr = {}
-  /** @type {NBTElementHelper$NBTListHelper} */// @ts-ignore
-  const att = horse.getNBT().get('attributes')
-  for (let a = 0; a < att.length(); a++)
-    // @ts-ignore
-    rawAttr[att.get(a).get('id').asString()] = att.get(a).get('base').asDouble()
-  if (!rawAttr['minecraft:generic.movement_speed']
-  ||  !rawAttr['minecraft:generic.jump_strength']
-  ||  !rawAttr['minecraft:generic.max_health'])
-    return keep = 8
-  const recognizableStat = { // convert attributes to recognizable format
-    speed: (42.16 * rawAttr['minecraft:generic.movement_speed'] - 0.001).toFixed(3),
-    jump: (-0.1817584952 * (rawAttr['minecraft:generic.jump_strength'] ** 3) + // wiki function
-            3.689713992  * (rawAttr['minecraft:generic.jump_strength'] ** 2) +
-            2.128599134  *  rawAttr['minecraft:generic.jump_strength'] - 0.343930367).toFixed(2),
-    health: rawAttr['minecraft:generic.max_health'].toFixed(1)
+    return
   }
   const percentStat = {
-    speed:  ((rawAttr['minecraft:generic.movement_speed'] - 0.1125) / 0.225 * 100).toFixed(1),
-    jump:   ((rawAttr['minecraft:generic.jump_strength']    - 0.4   ) / 0.6   * 100).toFixed(1),
-    health: ((rawAttr['minecraft:generic.max_health']     - 15    ) / 15    * 100).toFixed(1)
+    speed: toPercent(horse.getSpeedStat(), horse.getMinSpeedStat(), horse.getMaxSpeedStat()),
+    jump: toPercent(horse.getJumpStrengthStat(), horse.getMinJumpStrengthStat(), horse.getMaxJumpStrengthStat()),
+    health: toPercent(horse.getHealthStat(), horse.getMinHealthStat(), horse.getMaxHealthStat())
   }
-  const rarity = {
-    speed: 0,
-    jump: 0,
-    health: 0
+  const rarity = { speed: 0, jump: 0, health: 0 }
+  for (const n in rarity) {
+    rarity[n] = Math.max(0, rankGrading.findIndex(r => +percentStat[n] < r) - 1)
+    elem[`${n}Bar`].x2 = 24 + Math.floor(1.32 * +percentStat[n])
+    elem[`${n}Bar`].color = rankColor[rarity[n]] | 0xFF000000
+    elem[`${n}Text`].color = rankColor[rarity[n]]
   }
-  sjh.forEach(n => { // sort rarity
-    rankGrading.some((r, i) => +percentStat[n] >= r ? ((rarity[n] = 5 - i), true) : false)
-    if (!rarity[n]) rarity[n] = 0
-  })
-  Elem.background.color =
-  (rankColor[Math.floor(((rarity.speed ** 4 + rarity.jump ** 4 + rarity.health ** 4) / 3) ** 0.25)] >>> 2 & 0x3F3F3F) | 0xCF000000
-  Elem.title.setText(horse.getName())
-  Elem.speedText .setText(`Speed: ${recognizableStat.speed} m/s (${percentStat.speed}%)`)
-  Elem.jumpText  .setText(`Jump: ${recognizableStat.jump} blocks (${percentStat.jump}%)`)
-  Elem.healthText.setText(`Health: ${recognizableStat.health} (${percentStat.health}%)`)
-  if (!horseid) d2d.register()
-  sjh.forEach(n => {
-    Elem[`${n}Bar` ].x2 = orig.bar.orig + Math.floor(orig.bar.sizeMultiply * +percentStat[n])
-    Elem[`${n}Bar` ].color = rankColor[rarity[n]] | 0xFF000000
-    Elem[`${n}Text`].color = rankColor[rarity[n]]
-  })
-  horseid = horse.getUUID()
+  elem.background.color =
+    (rankColor[Math.floor(((rarity.speed ** 4 + rarity.jump ** 4 + rarity.health ** 4) / 3) ** 0.25)] >>> 2 & 0x3F3F3F) | 0xCF000000
+  elem.title.setText(horse.getName())
+  elem.speedText.setText(`Speed: ${(horse.getHorseSpeed() - 0.001).toFixed(3)} m/s (${percentStat.speed}%)`)
+  elem.jumpText.setText(`Jump: ${horse.getHorseJumpHeight().toFixed(2)} blocks (${percentStat.jump}%)`)
+  elem.healthText.setText(`Health: ${horse.getHealthStat().toFixed(1)} (${percentStat.health}%)`)
+  if (!lastHorse)
+    d2d.register()
+  lastHorse = horse
   keep = 8
 }))
 
-event.stopListener = JavaWrapper.methodToJava(() => {
-  if (horseid) d2d.unregister()
-})
+event.unregisterOnStop(true, d2d)
+
+/**
+ * @param {number} value  
+ * @param {number} min  
+ * @param {number} max  
+ */
+function toPercent(value, min, max, fixed = 1) {
+  return ((value - min) / (max - min) * 100).toFixed(fixed)
+}
